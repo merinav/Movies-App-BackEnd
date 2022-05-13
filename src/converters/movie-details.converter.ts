dd6a7f44045f2088c447f0ac312cece7d8074cb7
merinav <81377750+merinav@users.noreply.github.com>
2022-05-13 10:41:51 +0300

    feature/echo-13-movie-details-converter

--- src/converters/movie-details.converter.ts
+++ src/converters/movie-details.converter.ts
@@ -2,32 +2,32 @@ import { convertTmdbMovieToMovie } from './movie.converter';
 
 const IMG_PREFIX_ORIGINAL = 'https://image.tmdb.org/t/p/original';
 
-function convertTmdbMovieDetailstoMovieDetails(tmdbMovieDetails: TmdbMovieDetails): MovieDetails {
+const convertToProductionCountry = (tmdbProductionCountry: TmdbProductionCountry): ProductionCountry => {
+  return {
+    iso: tmdbProductionCountry.iso_3166_1,
+    name: tmdbProductionCountry.name,
+  };
+};
+
+const convertToSpokenLanguage = (tmdbSpokenLanguage: TmdbSpokenLanguage): SpokenLanguage => {
+  return {
+    englishName: tmdbSpokenLanguage.english_name,
+    iso: tmdbSpokenLanguage.iso_639_1,
+    name: tmdbSpokenLanguage.name,
+  };
+};
+
+const convertToProductionCompanies = (tmdbProductionCompanies: TmdbProductionCompany): ProductionCompany => {
+  return {
+    id: tmdbProductionCompanies.id,
+    logoPath: tmdbProductionCompanies.logo_path,
+    name: tmdbProductionCompanies.name,
+    originCountry: tmdbProductionCompanies.origin_country,
+  };
+};
+
+const convertTmdbMovieDetailstoMovieDetails = (tmdbMovieDetails: TmdbMovieDetails): MovieDetails => {
   const movie = convertTmdbMovieToMovie(tmdbMovieDetails);
-
-  const convertToProductionCountry = (tmdbProductionCountry: TmdbProductionCountry): ProductionCountry => {
-    return {
-      iso: tmdbProductionCountry.iso_3166_1,
-      name: tmdbProductionCountry.name,
-    };
-  };
-
-  const convertToSpokenLanguage = (tmdbSpokenLanguage: TmdbSpokenLanguage): SpokenLanguage => {
-    return {
-      englishName: tmdbSpokenLanguage.english_name,
-      iso: tmdbSpokenLanguage.iso_639_1,
-      name: tmdbSpokenLanguage.name,
-    };
-  };
-
-  const convertToProductionCompanies = (tmdbProductionCompanies: TmdbProductionCompany): ProductionCompany => {
-    return {
-      id: tmdbProductionCompanies.id,
-      logoPath: tmdbProductionCompanies.logo_path,
-      name: tmdbProductionCompanies.name,
-      originCountry: tmdbProductionCompanies.origin_country,
-    };
-  };
 
   const movieDetails: MovieDetails = {
     budget: tmdbMovieDetails.budget,
@@ -51,8 +51,7 @@ function convertTmdbMovieDetailstoMovieDetails(tmdbMovieDetails: TmdbMovieDetail
     posterPath: IMG_PREFIX_ORIGINAL + tmdbMovieDetails.poster_path,
     voteAverage: movie.voteAverage,
   };
-
   return movieDetails;
-}
+};
 
 export { convertTmdbMovieDetailstoMovieDetails };
